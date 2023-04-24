40d1be986e0e3b653a16d1a31418bcb2adf29d7d
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-04-23 23:42:59 -0400

    fix span doubling in DEDR parse

--- data/dedr/check_notes.py
+++ data/dedr/check_notes.py
@@ -5,9 +5,9 @@ notes = defaultdict(int)
 with open('dedr_new.csv', 'r') as f:
     read = csv.reader(f)
     for row in read:
-        # for item in row[6].split(' '):
-        if row[6]:
-            notes[(row[0], row[6])] += 1
+        for item in row[6].split(' '):
+            if row[6]:
+                notes[(row[0], row[6])] += 1
 
 # sort dict and print
 for k, v in sorted(notes.items(), key=lambda item: item[1]):
